bd68848bdaf0e8e8c83a1a136da47b33be90910b
DanyloPlotskykh <145497398+DanyloPlotskykh@users.noreply.github.com>
2024-03-02 00:21:14 +0200

    hellp meeeee

--- _server/network/HttpServer.cpp
+++ _server/network/HttpServer.cpp
@@ -7,8 +7,6 @@
 
 #define MAX_THREADS (std::thread::hardware_concurrency()) * 2
 
-namespace net = boost::asio;
-
 HttpServer::HttpServer(std::string addr, unsigned short port) : m_ioc{1},
                                                                 m_acc{m_ioc, net::ip::tcp::endpoint(net::ip::make_address(addr), static_cast<net::ip::port_type>(port))},
                                                                 m_thrpool{std::make_unique<ThreadPool>(MAX_THREADS)}
@@ -35,16 +33,17 @@ void HttpServer::do_accept()
     m_sock = boost::make_shared<net::ip::tcp::socket>(m_ioc);
     m_acc.accept(*m_sock);
     std::cout << "connecting" << std::endl;
-    m_thrpool->enqueue([&]()
-                       { on_accept(std::move(m_ec), std::move(m_sock)); });
+
+    // idk, but this connection doesent work at 2nd connection from client
+    //  m_thrpool->enqueue([this, sock]
+    //                     { on_accept(std::move(m_ec), std::move(sock)); });
+    on_accept(std::move(m_ec), std::move(m_sock));
 }
 
 void HttpServer::on_accept(boost::system::error_code ec, boost::shared_ptr<net::ip::tcp::socket> sock)
 {
     std::cout << "heard " << std::endl;
 
-    // work
-    std::this_thread::sleep_for(std::chrono::seconds(5));
     // start transaction))
     do_accept();
 }
