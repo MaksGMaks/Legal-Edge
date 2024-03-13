2874aa8420122359041772643ebf6ec8568365ba
DanyloPlotskykh <145497398+DanyloPlotskykh@users.noreply.github.com>
2024-03-13 22:44:25 +0200

    network worked correctly

---
+++ _client/network/NetworkService.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <QNetworkAccessManager>
+
+#include <QObject>
+#include <QHash>
+#include <QStringList>
+#include <QNetworkReply>
+
+using Data = QStringList;
+using Dataset = QMap<QString, Data>;
+
+enum class Method
+{
+    POST = QNetworkAccessManager::Operation::PostOperation,
+    PUT = QNetworkAccessManager::Operation::PutOperation,
+    GET = QNetworkAccessManager::Operation::GetOperation,
+    DEL = QNetworkAccessManager::Operation::DeleteOperation
+};
+
+class NetworkService : public QObject
+{
+    Q_OBJECT
+
+public:
+    NetworkService(QObject *parent = nullptr);
+
+    virtual void sendRequest(QString endpoint,
+                             const Method &method = Method::GET,
+                             const Dataset &dataset = Dataset(),
+                             const QString &id = "");
+
+    void setApiUrl(const QString &api);
+
+private slots:
+    void onNetworkReply();
+
+private:
+    QNetworkAccessManager *m_manager;
+    QString m_apiUrl;
+};
